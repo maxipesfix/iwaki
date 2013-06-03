f9aafc7ffbdd46174078095ae317ce51200c8563
Maxim <maxi@cmu.edu>
2013-06-03 00:23:46 -0400

    ros stub

---
+++ ros/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/maxim/h/iwaki/ros/devel/setup.sh"
+emulate zsh # back to zsh mode
